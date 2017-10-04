19307246a44a931d785b80cc3228d7aedb979d6f
TheOneVolodya <TheOneVolodya@mail.ru>
2017-10-04 19:27:47 +0400

    Шаг № 9

--- SWC_labwork_1/SWC_labwork_1/SWC_labwork_1.py
+++ SWC_labwork_1/SWC_labwork_1/SWC_labwork_1.py
@@ -1,9 +1,9 @@
 import math
 
-a = input("Введите значение А: ")
-b = input("Введите значение B: ")
-c = input("Введите значение С: ")
-D = b**2 - complex(4*a*c)
+a = complex(input("Введите значение А: "))
+b = complex(input("Введите значение B: "))
+c = complex(input("Введите значение С: "))
+D = b**2 - 4*a*c
 print("D = " + str(D))
 x1 = (-b+D**0.5)/(2*a)
 x2 = (-b-D**0.5)/(2*a)
